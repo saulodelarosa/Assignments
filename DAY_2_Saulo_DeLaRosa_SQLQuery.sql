95769ff5e69e9b79c74336181106f5bc4d0043da
saulodelarosa <108392308+saulodelarosa@users.noreply.github.com>
2022-06-30 22:37:18 -0400

    Day 2 sql file

---
+++ DAY_2_Saulo_DeLaRosa_SQLQuery.sql
@@ -0,0 +1,168 @@
+--1.
+SELECT COUNT(ProductID) AS [number of products in table]
+FROM Production.Product
+
+--2.
+SELECT COUNT(ProductID) AS [number of products in subcat]
+FROM Production.Product
+WHERE ProductSubcategoryID IS NOT NULL;
+
+--3.
+SELECT ProductSubcategoryID, COUNT(ProductID) AS [CountedProducts]
+FROM Production.Product
+WHERE ProductSubcategoryID IS NOT NULL
+GROUP BY ProductSubcategoryID;
+
+--4.
+SELECT COUNT(ProductID) AS [num without subcat]
+FROM Production.Product
+WHERE ProductSubcategoryID IS NULL;
+
+--5.
+SELECT SUM(Quantity) AS [total quantity of all prudcts]
+FROM Production.ProductInventory;
+
+--6.
+SELECT ProductID, SUM(Quantity) AS [TheSum]
+FROM Production.ProductInventory
+WHERE LocationID = 40
+GROUP BY ProductID
+HAVING  SUM(Quantity) < 100;
+
+--7.
+SELECT Shelf, ProductID, SUM(Quantity) AS [TheSum]
+FROM Production.ProductInventory
+WHERE LocationID = 40
+GROUP BY Shelf, ProductID
+HAVING  SUM(Quantity) < 100;
+
+--8.
+SELECT  AVG(Quantity) AS [Average quantity]
+FROM Production.ProductInventory
+WHERE LocationID = 10
+
+
+--9.
+SELECT ProductID, Shelf, AVG(Quantity) AS [TheAvg]
+FROM Production.ProductInventory
+GROUP BY Shelf, ProductID
+ORDER BY Shelf ASC;
+
+--10.
+SELECT ProductID, Shelf, AVG(Quantity) AS [TheAvg]
+FROM Production.ProductInventory
+WHERE Shelf != 'N/A'
+GROUP BY Shelf, ProductID
+ORDER BY Shelf ASC;
+
+--11.
+SELECT Color, Class, COUNT(ProductID) AS [TheCount], AVG(StandardCost) AS [TheAvg]
+FROM Production.Product
+WHERE (Color IS NOT NULL) AND (Class IS NOT NULL)
+GROUP BY Color, Class
+
+--12.
+SELECT c.name AS [Country], s.name AS [Province]
+FROM Person.CountryRegion AS c
+JOIN Person.StateProvince AS s ON c.CountryRegionCode = s.CountryRegionCode
+
+--13.
+SELECT c.name AS [Country], s.name AS [Province]
+FROM Person.CountryRegion AS c
+JOIN Person.StateProvince AS s ON c.CountryRegionCode = s.CountryRegionCode
+WHERE (c.name = 'Germany') OR c.name = 'Canada'
+
+--Using Northwnd Database
+
+--14.
+SELECT p.ProductName, SUM(od.Quantity) AS [Number sold in past 25 years.]
+FROM dbo.Products AS p
+JOIN dbo.[Order Details] AS od ON p.ProductID = od.ProductID
+JOIN dbo.Orders AS o ON od.OrderID = o.OrderID
+WHERE (o.ShipPostalCode IS NOT NULL) AND (DATEDIFF(year, o.OrderDate, GETDATE())< 25)
+GROUP BY p.ProductName
+ORDER BY SUM(od.Quantity) DESC
+
+--15.
+SELECT TOP 5 o.ShipPostalCode, SUM(od.Quantity) AS [Number sold]
+FROM dbo.[Order Details] AS od
+JOIN dbo.Orders AS o ON od.OrderID = o.OrderID
+WHERE o.ShipPostalCode IS NOT NULL
+GROUP BY o.ShipPostalCode
+ORDER BY SUM(od.Quantity) DESC
+
+--16.
+SELECT TOP 5 o.ShipPostalCode, SUM(od.Quantity) AS [Number sold]
+FROM dbo.[Order Details] AS od
+JOIN dbo.Orders AS o ON od.OrderID = o.OrderID
+WHERE (o.ShipPostalCode IS NOT NULL) AND (DATEDIFF(year, o.OrderDate, GETDATE())< 25)
+GROUP BY o.ShipPostalCode
+ORDER BY SUM(od.Quantity) DESC
+
+--17.
+SELECT City, COUNT(CustomerID)
+FROM customers
+WHERE City IS  NOT NULL
+GROUP BY City 
+ORDER BY 2 DESC;
+
+--18.
+SELECT City, COUNT(CustomerID)
+FROM customers
+WHERE City IS  NOT NULL
+GROUP BY City 
+HAVING COUNT(CustomerID) > 2
+ORDER BY 2 DESC;
+
+--19.
+SELECT ContactName, o.OrderDate 
+FROM Customers AS [c]
+JOIN Orders AS [o] ON c.CustomerID = o.CustomerID
+WHERE OrderDate > '01.01.1998 00:00:00'
+
+--20.
+SELECT ContactName, o.OrderDate 
+FROM Customers AS [c]
+JOIN Orders AS [o] ON c.CustomerID = o.CustomerID
+ORDER BY OrderDate DESC;
+
+--21.
+SELECT ContactName, SUM(od.Quantity) AS [total products bought]
+FROM Customers AS [c]
+JOIN Orders AS [o] ON c.CustomerID = o.CustomerID
+JOIN [Order Details] AS [od] ON o.OrderID = od.OrderID
+GROUP BY ContactName
+ORDER BY SUM(od.Quantity) DESC
+
+--22.
+SELECT c.CustomerID, SUM(od.Quantity) AS [total products bought]
+FROM Customers AS [c]
+JOIN Orders AS [o] ON c.CustomerID = o.CustomerID
+JOIN [Order Details] AS [od] ON o.OrderID = od.OrderID
+GROUP BY c.CustomerID
+HAVING  SUM(od.Quantity) > 100
+ORDER BY SUM(od.Quantity) DESC
+
+--23.
+SELECT s.CompanyName, ship.CompanyName
+FROM Products AS [p]
+JOIN Suppliers AS [s] ON p.SupplierID = s.SupplierID
+JOIN [Order Details] AS [od] ON p.ProductID = od.ProductID
+JOIN Orders AS [o] ON od.OrderID = o.OrderID
+CROSS JOIN Shippers as [Ship]
+
+--24.
+SELECT p.ProductName, o.OrderDate
+FROM Products AS [p]
+JOIN [Order Details] AS [od] ON p.ProductID = od.ProductID
+JOIN Orders AS [o] ON od.OrderID = o.OrderID
+
+--25.
+SELECT E1.FirstName + E1.LastName [EmployeeName 1], E2.FirstName + E2.LastName [EmployeeName 2]
+FROM Employees [E1]
+JOIN Employees as [E2] ON E1.Title = E2.Title 
+WHERE (E1.FirstName + E1.LastName != E2.FirstName + E2.LastName)
+
+
+
+
